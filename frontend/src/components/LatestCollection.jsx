f075b3b203d36ed2d65998ed4ad60485ed5f9ff0
Your Full Name <ehasun0909@gamil.com>
2025-06-13 22:57:34 +0600

    13june-2025

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,9 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const LatestCollection = () => {
   const { products } = useContext(ShopContext);
+
+  const [latestProduct, SetLatestProduct] = useState([]);
+
+  useEffect(() => {
+    SetLatestProduct(products.slice(0, 10));
+  }, []);
 
   return (
     <div className="my-10">
@@ -12,6 +19,19 @@ const LatestCollection = () => {
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit.
         </p>
+      </div>
+
+      {/* Rendering Products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProduct.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item.id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
       </div>
     </div>
   );
