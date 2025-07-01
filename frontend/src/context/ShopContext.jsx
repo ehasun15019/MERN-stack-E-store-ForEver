fa84486c2596833ac15077b2c80e3b4ad10465bb
Your Full Name <ehasun0909@gamil.com>
2025-07-02 01:33:59 +0600

    02July

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,18 +1,27 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
 const ShopContentProver = (props) => {
   const currency = "$";
   const delivery_fee = 10;
+
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
+  const navigate = useNavigate();
 
-  // add to cart function
+  // Add to cart
   const addToCart = async (itemId, size) => {
     let cartData = structuredClone(cartItems);
+
+    if (!size) {
+      toast.error("Select Product Size");
+      return;
+    }
 
     if (cartData[itemId]) {
       if (cartData[itemId][size]) {
@@ -28,9 +37,57 @@ const ShopContentProver = (props) => {
     setCartItems(cartData);
   };
 
+  // Log cart updates
   useEffect(() => {
     console.log(cartItems);
   }, [cartItems]);
+
+  // Get total cart count for icon
+  const getCartCount = () => {
+    let totalCount = 0;
+
+    for (const itemId in cartItems) {
+      for (const size in cartItems[itemId]) {
+        try {
+          if (cartItems[itemId][size] > 0) {
+            totalCount += cartItems[itemId][size];
+          }
+        } catch (error) {
+          console.error("Error while calculating cart count:", error);
+        }
+      }
+    }
+
+    return totalCount;
+  };
+
+  // Update item quantity
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId][size] = quantity;
+    setCartItems(cartData);
+  };
+
+  //
+  const getCartAmount = () => {
+    let totalAmount = 0;
+
+    for (const items in cartItems) {
+      let iteInfo = products.find((product) => product._id === items);
+
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalAmount += iteInfo.price * cartItems[items][item];
+          }
+        } catch (error) {
+          console.error("Error while calculating cart Amount:", error);
+        }
+      }
+    }
+
+    return totalAmount;
+  };
 
   const value = {
     products,
@@ -42,6 +99,10 @@ const ShopContentProver = (props) => {
     setShowSearch,
     cartItems,
     addToCart,
+    getCartCount,
+    updateQuantity,
+    getCartAmount,
+    navigate,
   };
 
   return (
--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,9 +1,69 @@
 import React from "react";
+import Title from "../components/Title";
 
 const PlaceOrder = () => {
   return (
-    <div>
-      <h2>Place Order Page</h2>
+    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
+      {/* -------------- Left side -------------- */}
+      <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
+        <div className="text-xl sm:text-2xl my-3">
+          <Title text1={"DELIVERY"} text2={"INFORMATION"} />
+        </div>
+
+        <div className="flex gap-3">
+          <input
+            type="text"
+            placeholder="First name"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+
+          <input
+            type="text"
+            placeholder="Last name"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+        </div>
+
+        <input
+          type="email"
+          placeholder="Email address"
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+        />
+
+        <input
+          type="text"
+          placeholder="Street"
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+        />
+
+        <div className="flex gap-3">
+          <input
+            type="text"
+            placeholder="City"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+
+          <input
+            type="text"
+            placeholder="State"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+        </div>
+
+        <div className="flex gap-3">
+          <input
+            type="number"
+            placeholder="Zipcode"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+
+          <input
+            type="text"
+            placeholder="Country"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+        </div>
+      </div>
     </div>
   );
 };
