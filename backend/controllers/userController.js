5f858bea463dc7a008b7517e85a1b26528d59462
Your Full Name <ehasun0909@gamil.com>
2025-07-12 20:55:50 +0600

    added userController.js in controllers folder for login, register/signup and admin-login

---
+++ backend/controllers/userController.js
@@ -0,0 +1,18 @@
+
+// Route for user login
+const loginUser = async (req, res) => {
+
+}
+
+
+//  Route for user Remigration
+const registerUser = async (req, res) => {
+
+}
+
+// Router for admin login
+const adminlogin = async (req, res) => {
+
+}
+
+export { loginUser, registerUser }
